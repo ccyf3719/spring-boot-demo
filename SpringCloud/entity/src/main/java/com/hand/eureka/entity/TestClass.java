6c81a0084fa2cfe16133efd6b598c97f9edac3ec
炊逸飞 <YIFEI.CUAN@hand-china.com>
2020-08-26 10:34:50 +0800

    spring cloud

---
+++ SpringCloud/entity/src/main/java/com/hand/eureka/entity/TestClass.java
@@ -0,0 +1,15 @@
+package com.hand.eureka.entity;
+
+/**
+ * description
+ *
+ * @author yifei.cuan@hand-china.com 2020/08/07 10:04
+ */
+public class TestClass {
+
+	public static void main(String[] args) {
+		//参数不足八位的补零
+		System.out.println(String.format("%08d",123));
+	}
+
+}
